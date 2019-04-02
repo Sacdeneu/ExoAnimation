3162d5224eaa593174967deae86f5a3d7dc5b587
Alexandre Cassanelli <sacdeneu@gmail.com>
2019-04-02 14:47:34 -0400

    first push

---
+++ eric_laberge-8trd150-20191-hiver-835de973ca5b/Assets/Exercices/5/SpriteAnimation.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+[RequireComponent(typeof(RectTransform))]
+[RequireComponent(typeof(RawImage))]
+public class SpriteAnimation : MonoBehaviour
+{
+    public SpritesheetDescription spritesheet;
+    public string sprite;
+    public string defaultAnimation;
+    public float frameLength;
+    public AnimationScenario scenario;
+
+    private Queue<AnimationScenario.SequenceItem> sequence;
+    private AnimationScenario.SequenceItem currentAction;
+    private float elapsed;
+    private string anim;
+    private int frame;
+
+    private RectTransform rectTransform;
+    private RawImage image;
+    private Texture2D spritesheetTexture;
+
+    public void Start()
+    {
+        rectTransform = GetComponent<RectTransform>();
+        image = GetComponent<RawImage>();
+        spritesheetTexture = image.texture as Texture2D;
+
+        sequence = new Queue<AnimationScenario.SequenceItem>(scenario.sequence);
+        anim = defaultAnimation;
+    }
+
+    public void Update()
+    {
+        if (sequence.Count > 0 && elapsed >= sequence.Peek().time)
+        {
+            currentAction = sequence.Dequeue();
+        }
+
+        elapsed += Time.deltaTime;
+        DoAction();
+
+        var frameDesc = spritesheet.GetFrame(GetFrameName());
+
+        image.uvRect = new Rect(0.0f, 0.0f, 1.0f, 1.0f);
+    }
+
+
+    void DoAction()
+    {
+        if (currentAction.setAnim)
+        {
+            SetAnim(currentAction.newAnim);
+        }
+
+        rectTransform.anchoredPosition += currentAction.motion * Time.deltaTime;
+    }
+
+    string GetFrameName()
+    {
+        return string.Format("{0}/{1}/{2}", sprite, anim, frame);
+    }
+
+    void SetAnim(string newAnim)
+    {
+        if (anim == newAnim)
+            return;
+
+        anim = newAnim;
+        frame = 0;
+    }
+}
---
+++ eric_laberge-8trd150-20191-hiver-835de973ca5b/Assets/Exercices/6/TentacleAnimation.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(TentacleSkeleton))]
+public class TentacleAnimation : MonoBehaviour
+{
+    public float[] rotations;
+
+    private TentacleSkeleton skeleton;
+
+    public void Start()
+    {
+        skeleton = GetComponent<TentacleSkeleton>();
+    }
+
+    public void Update()
+    {
+        for (int i = 0; i < rotations.Length; i++)
+        {
+            skeleton.SetRotation(i, Quaternion.Euler(0f, 0f, rotations[i]));
+        }
+    }
+}
